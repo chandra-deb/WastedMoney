a4286b2a7f7f8ce16a386448bb31f40473b7feef
CHANDRA <chandra@pop-os.localdomain>
2020-10-23 17:22:59 +0600

    initial setup

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WastedMoney',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Wasted Money'),
+      ),
+      body: Center(
+        child: Text(
+          'Hello World!',
+        ),
+      ),
+    );
+  }
+}
